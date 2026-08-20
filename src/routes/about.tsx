6d2c9749328a8312bc3d5dadaf4d16d4e02c3bca
faizan1405 <faizankhan1405@gmail.com>
2026-08-20 23:07:24 +0530

    x

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -105,7 +105,7 @@ function About() {
       <section className="mx-auto mt-32 max-w-[1400px] px-5 lg:px-16">
         <div className="grid items-center gap-12 lg:grid-cols-[0.85fr_1.15fr] lg:gap-16">
           <RevealImage
-            src="/assets/founder.jpg"
+            src="https://pub-a9ee4b9e6d764ca88dc8d5f3776c28e2.r2.dev/assets/founder.jpg"
             alt="Founder of Clickographers Wedding Films"
             className="aspect-[3/4] w-full"
           />
