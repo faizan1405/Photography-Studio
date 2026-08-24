c471f460113a2a99de303a66d0359bf557031827
faizan1405 <faizankhan1405@gmail.com>
2026-08-25 01:43:09 +0530

    s

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -63,7 +63,7 @@ function About() {
       <Header />
 
       <PageHero
-        src={`${R2_BASE}/ring/1122.jpg`}
+        src={`${R2_BASE}/wedding/DSC05872.jpg`}
         alt="Luxury wedding mandap decorated with flowers at dusk"
         eyebrow="Our Story"
         title="Twelve years of standing quietly in the middle of joy"
